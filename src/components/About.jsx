9a82662e0fd5614b18084e2a4640aa7b8d9edf72
jlouis407 <josiah.israel.louis@gmail.com>
2021-05-18 20:30:56 -0400

    add content to About and Header components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,17 @@ function About(){
                     I am a recent graduate of Valencia College (Orlando, Florida). I have a 
                     Bachelor of Applied Science in Computing Technology & Software Development.
                     I'm very eager to put my education to use in a productive tech career.
+                    <br/>
+                    <br/>
+                    My main skillset is in web development technologies, namely HTML, CSS, and JavaScript. 
+                    I'm also knowledgeable about back-end materials like Node.JS, Mongoose, and SQL. Others skills
+                    include object-oriented programming, Express, and Git/Version Control.
+                    <br/>
+                    <br/>
+                    Email: jlouis2016@gmail.com <br/>
+                    Cell: 321-318-9857
                 </p>
+    
             </div>
             <div class="column">
                 <img 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,11 +6,8 @@ function Header(){
             <div className="header-navbar-div">
                 <div className="header-logo-div">
                     <a href="/" className="header-logo">Josiah</a>
-                </div>
-                    <a href="/" className="navbar-item">Home</a>
-                    <a href="/portfolio" className="navbar-item">My Portfolio</a>
-                    <a href="/contact" className="navbar-item">Contact Me</a>
-                </div>
+              </div>
+            </div>
         </div>
             
     );
